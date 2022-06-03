d94ffffd34f93ce37050ec35c6bd2c4cc162aaee
victorolivaag <104523612+victorolivaag@users.noreply.github.com>
2022-06-03 09:14:45 -0500

    Add files via upload

---
+++ Python/2ndo Parcial/Challenge Elif 2.py
@@ -0,0 +1,9 @@
+x=int(input("Ingresa la velocidad del meteorito: "))
+if (x>=25):
+  print('alerta critica')
+elif (x==20):
+  print('alerta de rayo')
+elif (x>=10):
+  print("estrella fugaz")
+else:
+  print("nada que ver")

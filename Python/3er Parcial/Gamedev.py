90727eec9492fdf92de1b3443559405553a2e78f
victorolivaag <104523612+victorolivaag@users.noreply.github.com>
2022-06-29 09:19:04 -0500

    Add files via upload

---
+++ Python/3er Parcial/Gamedev.py
@@ -0,0 +1,449 @@
+
+import time
+while True:
+  start=input("Click enter para iniciar ")
+  print("L A   L E C H E R A")
+  input()
+  
+  print("En este juego puede terminar asi:")
+  
+  print("""
+              ~(^^^^)~
+               ) @@ \~_          |\
+              /     | \        \~ /
+             ( 0  0  ) \        | |
+              ---___/~  \       | |
+               /'__/ |   ~-_____/ |
+o          _   ~----~      ___---~
+  O       //     |         |
+         ((~\  _|         -|
+   o  O //-_ \/ |        ~  |
+        ^   \_ /         ~  |
+               |          ~ |
+               |     /     ~ |
+               |     (       |
+                \     \      /\
+               / -_____-\   \ ~~-*
+               |  /       \  \
+               / /         / /
+             /~  |       /~  |
+             ~~~~        ~~~~
+ """)
+  print("Una lechera pobre que consume drugs o...")
+  input()
+  
+  print("Siendo una lechera millonaria")
+  input()
+  print("""
+             _\,;;;;;;;,/_
+          .-"; ;;;;;;;;; ;"-.   [̲̅$̲̅(100)̲̅$̲̅]
+          '.__/`_ / \ _`\__.' 
+[̲̅$̲̅(100)̲̅$̲̅]    | (')| |(') |                 [̲̅$̲̅(100)̲̅$̲̅]
+             | .--' '--. |
+             |/ o     o \|     [̲̅$̲̅(100)̲̅$̲̅      [̲̅$̲̅(100)̲̅$̲̅]    |           |
+            / \ _..=.._ / \
+           /:. '._____.'   \              [̲̅$̲̅(100)̲̅$̲̅]     
+          ;::'    / \      .;
+[̲̅$̲̅(100)̲̅$̲̅] |     _|_ _|_   ::|
+        .-|     '==o=='    '|-.    [̲̅$̲̅(100)̲̅$̲̅]
+       /  |  . /       \    |  \                 
+       |  | ::|         |   | .|
+       |  (  ')         (.  )::|          [̲̅$̲̅(100)̲̅$̲̅]
+       |: |   |; U U U ;|:: | `|
+       |' |   | \ U U / |'  |  |                  
+       ##V|   |_/`''''`\_|   |V##                [̲̅$̲̅(100)̲̅$̲̅]
+           ##V##         ##V##       [̲̅$̲̅(100)̲̅$̲̅]
+""")
+
+  
+  print("Contexto:")
+  time.sleep(1)
+  print("*Una voz empieza a sonar en su cabeza y le dice...*")
+  input()
+  print("*Hoy es un dia muy lleno de trabajo para ti...*")
+  input()
+  print("*Eres una lechera, quieres ser multimillonaria y ser dueña de KFC* ")
+  input()
+  print("*Pero todo lo que hagas y decidas el dia de hoy será lo que decida tu futuro... ")
+  input()
+
+  
+  print("Un día a las  7:00 am")
+  time.sleep(1)
+  print("El despertador suena...")
+  input()
+  print ("*BEEEEP BEEEEP BEEEEP*")
+  input()
+  print("Quieres que se despierte?")
+  print("a. Si")
+  print("b. No")
+  q1=input("Que decides? ")
+
+  
+  if (q1== "a"):
+    print("*Te levantas lentamente...*")
+    time.sleep(1)
+    print("*No te acuerdas de tu nombre*")
+    print("¿Cual es mi nombre?")
+    time.sleep(1)
+    nombre=input("ingresa tu nombre: ")
+    print("Ya recuerdo...")
+    input()
+    print("Me llamo", nombre)
+    time.sleep(2)
+    print("Me iré a alistar, yo se que hoy será un muy buen dia")
+    input()
+    
+    print("*Se alista para salir a vender leche*")
+    time.sleep(1)
+    print("*Se va en burro a la Central de Abastos a vender a leche* ")
+    input()
+    print("*Llega a la Central")
+    input()
+    print("*Va a su puesto*")
+   
+    time.sleep(1)
+    print("*Unos minutos despues llega su primer cliente*")
+    input()
+    print("*Atiende de buena manera al cliente*")
+    print("*Ella no sabia que el cliente era el dueño de KFC*")
+    input()
+    print("*Y se enamora a primera vista del cliente*")
+    input()
+
+    
+    time.sleep(2)
+    print("-",nombre,":Hola, que necesita?")
+    input()
+    print("- Harland Sanders: Hola, quiero 2L de leche porfavor")
+    input()
+    print("-",nombre,": Ok, seria todo?")
+    input()
+    print("- Harland Sanders: Si ")
+    input()
+    print("-",nombre, ": Ok, serian $50 pesos")
+    input()
+    print("*Harland le da un billete de $100* ")
+    input()
+    print(nombre, "*Le da el cambio*")
+    input()
+    print("- Harland Sanders: Muchas gracias ")
+    input()
+    print("*Se va*")
+    time.sleep(3)
+    print("*Empiezan a llegar más clientes, despues de un buen rato decides irte a descansar*")
+    input()
+    print("-",nombre,": Hoy ha sido un muy buen día")
+    input()
+    print("-",nombre, ": He ganado un total de $2,500")
+    input()
+
+    
+    print("*Cierra su local*")
+    input()
+    print("*Mientras", nombre, "se va hacia su burro se encuentra con Harland Sanders, y empiezan a platicar*")
+    input()
+    print("- Harland Sanders: Hola soy Harland, te compre leche hace rato")
+    input()
+    print("-",nombre,":Ahh si, hola me llamo", nombre)
+    input()
+    print("-Harland Sanders: Que bonito nombre")
+    input()
+    print("-",nombre,": Ayy gracias (se pone roja)")
+    print("-Harland Sanders: Ya te vas?")
+    input()
+    print("-",nombre,": Si ya, hoy fue un día muy largo") 
+    input()
+    print("-Harlands Sanders: Nos podemos ver por la tarde para ver tu granja?")
+    input()
+    print("-",nombre,": Sii! Nos vemos a las 5:00")
+    input()
+
+    
+    print("- Harland Sanders: Oye me puedes pasar tu número de telefono?")
+    input()
+    print("-", nombre,": (se pone nerviosa), Para que?")
+    input()
+    print("-Harland Sanders: Jajjaja, para que me pases ubi de tu granja")
+    input()
+    print("-",nombre,": A si claro, mi número es 2245393107")
+    input()
+    print("-Harland Sanders: Ok, en un rato te escribo, cuando este libre para que me pasas la ubi va?")
+    input()
+    print("-",nombre,": A si claro, me avisas para que te la mande")
+    input()
+    print("-Harland Sanders: Adios")
+    input()
+    print("-",nombre,":Nos vemos luego")
+    input()
+
+    
+    time.sleep(2)
+    print("- * 2:00 DE LA TARDE * -")
+    input()
+    print(" - * Te marca Hardland Sanders * - ")
+    q2=input("Le contestaré?... o no??? ")
+    print("a. Si")
+    print("b. No")
+    q2=input("Que decides? ")
+
+    
+    if (q2== "a"):
+      print("*Espera unos seg para contestar*")
+      input()
+      print("*Le contesta*")
+      input()
+      print("- Harland Sanders: Buenoooo?... Bueeeeenooooo?")
+      input()
+      print("-", nombre, ":Buenooooo?...Buenooooooooo?")
+      input()
+      print("-Harland Sanders: No te escuchooooo")
+      input()
+      print("-", nombre, ": Ya me escuchass?")
+      input()
+      print("-Harland Sanders: Yaa, al fin")
+      input()
+      print("-",nombre,": Que necesitas?")
+      input()
+      print("-Harland Sanders: Se te olvido?...")
+      input()
+      print("-",nombre,"Que?  A ya se me había olvidado, ahorita mando la dirreción")
+      input()
+      print("-Harland Sanders: Ok")
+      input()
+      print("-",nombre,":*Se la manda*")
+      input()
+
+    
+      time.sleep(2)
+      print("*Mientras espera a que llegue, jugara preguntados*")
+      input()
+      print("~Bienvenido a preguntados~")
+      input()
+      print("Cual ")
+      
+      print("*Llega Harland Sanders*")
+      input()
+      print("-Harland Sanders: Hola, fue dificil llegar ")
+      input()
+      print("-",nombre,": Por que?? ")
+      input()
+      print("-Harland Sanders: Porque para venir la carretera esta muy peligrosa, tiene muchas curvas")
+      input()
+      print("-",nombre,": Cierto, pero como yo vengo a la granja en burro, no lo siento :)")
+      input()
+      print("-Harland Sanders: Como que en burro?")
+      print("-",nombre,": Si, no tengo auto, solo tengo un burro")
+      input()
+      print("-Harland Sanders: (cara en shock), como puedes transladarte en burro?")
+      input()
+      print("-",nombre,": Jajajaja si, dejemos este tema y vamos a ver la granja")
+      input()
+
+      print("""
+         /')
+  ////  /' )'
+ @   \/'  )'
+< (  (_...)'
+  \      )
+   \,,,,/
+     _|_
+           """)
+
+      
+      
+      print("-Harland Sanders: Esta bien, quiero ver la parte de los pollos y me voy")
+      input()
+      print("-", nombre, ": No vas a ver toda la granja?")
+      input()
+      print("-Harland Sanders: No se si sabes pero yo soy el dueño del KFC y obvio necesito pollos para vender")
+      input()
+      print("-",nombre,": Usted queee??? (se pone palida)")
+      input()
+      print("-Harland Sanders: Al parecer no sabias")
+      input()
+      print("-",nombre,": Nooo")
+      input()
+
+      time.sleep(2)
+      
+      print("-Harland Sanders: Bueno dejemos eso para luego, vamos a ver los pollos")
+      input()
+      print("-",nombre,": Sigueme, y cuidado porque hay mucha piedra")
+      input()
+      
+      print("""
+          .-"-.
+         /  _  \
+         | / \ |
+        _|_|_|_|_
+"--...-".     .  "---...--""
+            
+            """)
+      input()
+      
+      print("Mira aqui estan los pollos:")
+      input()
+      
+      print("""
+              .=""=.              .=""=.
+             / _  _ \            / _  _ \
+            |  d  b  |          |  d  b  |
+            \   /\   /          \   /\   /
+           ,/'-=\/=-'\,        ,/'-=\/=-'\,
+          / /        \ \      / /        \ \
+         | /          \ |    | /          \ |
+         \/ \        / \/    \/ \        / \/
+             '.    .'            '.    .'
+             _|`~~`|_            _|`~~`|_
+   ^^^~^^^~^~/|\^~/|\^^~^^~^~^^^~/|\^^/|\~^~^~^~^~^~^
+      """)
+      print("CONTINUARA...")
+      
+      
+      
+  if (q1=="b"):  
+    print("No")
+    input("*Suena el despertador*")
+    input("*Apagas el despertador*")
+    time.sleep(2)
+
+     
+    print("5  H O R A S  D E S P U E S...")
+    time.sleep(1)
+    print("Demonios! Me quedé dormida")
+    input()
+    print("Que pasa...")
+    input()
+    print("*No te acuerdas de tu nombre*")
+    input()
+    print("No recuerdo mi nombre...")
+    input()
+    
+   
+    time.sleep(1)
+    nombre=input("Ingresa tu nombre: ")
+    print("Ya recuerdo...")
+    input()
+    print("me llamo", nombre)
+
+    
+    time.sleep(1)
+    print(nombre, "- Ohhh f***! Ya no fui a trabajar!" )
+    input()
+    print("-",nombre,": Pues ya, ya no fui, x, que se hace, llorar?... ") 
+    print("No")
+    input()
+    print(" - * se arregla para salir a pasear * -")
+  
+    input()
+    print("-",nombre,": hoy traigo ganas de gastar dinero... pero cuanto tendré ahorrado?") 
+    input()
+    print("- * checha su cartera * -") 
+    input()
+    print("-", nombre,":Nada más tengo $5000 ahorrados, que puedo hacer con eso?... ")
+    input()
+    print("Alcohol?.... Drogas?")
+    input()
+    
+    print("-",nombre,":*Caminare a ver si encuentro en donde comprar drogas, porque el acohol en cualquier lugar se consigue")
+    input()
+
+    print("No, mejor primero por alcohol porque es mas facil")
+    input()
+    print("*Va a comprar alcohol*")
+    input()
+    print("""
+  
+          
+          *                           )   *
+              )     *      (
+    )        (                   (
+   (          )     (             )
+    )    *           )        )  (
+   (                (        (      *
+    )          H     )        )
+              [ ]            (
+       (  *   |-|       *     )    (
+ *      )     |_|        .          )
+       (      | |    .  
+ )           /   \     .    ' .        *
+(           |_____|  '  .    .  
+ )          | ___ |  \~~~/  ' .   (
+        *   | \ / |   \_/  \~~~/   )
+            | _Y_ |    |    \_/   (
+*           |-----|  __|__   |      *
+            `-----`        __|__
+         
+         """)
+
+  print("-", nombre,": *Va a buscar un bar*")
+  print(""" 
+       
+                    _ ___ _  _ _   _
+                    |\  /||(_  | |_)|_(_  (_
+                    | \/ ||__) | | \|___) __)
+                    __    _   _ ___      _, _
+                   /  |_||_)|(_  | ||\ |(_ (_
+                   \__| || \|__) | || \|(_ __)
+                   ||          Bar          ||
+   ________________||_______________________||_____________
+  |_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_||
+  |_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|| /|
+  |_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_||/||
+  |_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|||/|
+  |_|_|_|_|_|_|_|_|_|     _      _     |_|_|_|_|_|_|_|_|_|_|/||
+  |_|               |    (_)    (_)    |                 |_|/||
+  |_|.              |__________________|     (^^)  .     |_||/|
+  |_|*`.            |_|      ||      |_|     _)(_  I`.   |_|/||
+  |_| S `.          |_| llup || push |_|    /|  |\/| ;   |_||/|
+  |_|`. A `.        |_| tuo  ||  in  |_|   // )_\\/      |_|/||
+  |_|  `. L `.      |_|     [||]     |_|   \|.//_)       |_||/|
+  |_|    `. E `.    |_|      ||      |_|     // /        |_|/||
+  |_|______`__*_`___|_|      ||      |_|_____\\|_________|_||/|
+  |_|_|_|_|_|_|_|_|_|_|______||______|_|_|_|_|_|_|_|_|_|_|_|/||
+__|_|_|_|_|_|_|_|_|_|_|______||______|_|_|_|_|_|_|_|_|_|_|_||/________
+ /     /     /     /     /     /     /     /     /     /     /     /
+/_____/_____/_____/_____/_____/_____/_____/_____/_____/_____/_____/
+___________________________________________________________________
+ /_/_/_/
+       
+       
+              """)
+  print(nombre, "llega al bar...")
+  input("Hola, me podria dar 15 shots de tequila porfavor?")
+  input("Mesero: Si, claro. Viene acompañada? O por que tantos shots?")
+  print(nombre,": Nono, ya sabe, dia malo. Hay que quitar y sacar las penas con alcohol...")
+  print(nombre,": me acompaña a sufrir? ;(")
+  input("Mesero: Bueeeeno, puede que me corran del trabajo pero yo tambien tengo de que sufrir, asi que va.")
+  input("-empiezan a tomar-")
+  time.sleep(2)
+  print("-- 2 horas depsues --")
+  input()
+  print("Mesero: ¡HEY, HEY!",nombre)
+  input("Mesero: YA DEJA DE TOMAR! LLEVAS 32 SHOTS!")
+  q4=input("¿Quieres seguir tomando?")
+  print("a) si")
+  print("b) no")
+  if (q4=="b"):  
+     print("No")
+     print(nombre,": Está bien, dejare de tomar y me ire a mi casa")
+     print(nombre,": muchas gracias por todo, a ver si luego quedamos en algun lugar y nos conocemos mejor")
+     input(nombre,"se va a su casa..")
+  time.sleep(2)
+  print("-SE VA A SU CASA TRISTE-")
+  time.sleep(2)
+  print(" C O N T I N U A R A ....")
+  if (q4=="a"):  
+     print(nombre,": no importa, seguire tomando.")
+  print(nombre,": es que no, no es suficiente, ya no aguanto mi vida")
+  input()
+  print(nombre,"-agarra una botella de la barra y se la empina-")
+  input()
+  print(nombre,"-le da un coma elitico-")
+  input("Mesero: ESTAS BIEN? OYE, HEYYY! HEYYYY! QUE ONDAAAA!")
+  input(" C O N T I N U A R A ...")
+  
+  
+  
+  
